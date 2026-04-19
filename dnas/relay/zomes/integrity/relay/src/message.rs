673b788f54ff194662f3eb646ffacbc979f9c303
MasterK0927 <keshav.agarwal.12325@gmail.com>
2026-04-20 02:37:44 +0530

    feat: add MessageType enum to Message struct for system messages

--- dnas/relay/zomes/integrity/relay/src/message.rs
+++ dnas/relay/zomes/integrity/relay/src/message.rs
@@ -1,5 +1,11 @@
 use hdi::prelude::*;
 
+#[derive(Serialize, Deserialize, Debug, Clone, SerializedBytes, PartialEq)]
+pub enum MessageType {
+    User,
+    System,
+}
+
 #[derive(Serialize, Deserialize, Debug, SerializedBytes, Clone, PartialEq)]
 pub struct File {
     pub name: String,
@@ -15,6 +21,7 @@ pub struct Message {
     pub content: String,
     pub bucket: u32,
     pub images: Vec<File>,
+    pub message_type: MessageType,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
