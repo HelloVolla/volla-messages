ea743dd48ae5218e6cdff3f5f5820e2d427c1fe1
Amirhesp <amir.h.esmailpor@gmail.com>
2026-06-01 18:05:05 +0330

    Build: release new version of the volla-messages v0.12.3

--- src-tauri/gen/android-holochain_bundled/app/build.gradle.kts
+++ src-tauri/gen/android-holochain_bundled/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.volla.messages.bundled"
         minSdk = 27
         targetSdk = 34
-        versionCode = 12002
-        versionName = "0.12.2"
+        versionCode = 12003
+        versionName = "0.12.3"
         // versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         // versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
