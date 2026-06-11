84dfa513fe74c57447e67513b3b14b644836af3e
Amirhesp <amir.h.esmailpor@gmail.com>
2026-06-11 15:14:14 +0330

    Build: move to v0.12.4

--- src-tauri/gen/android-holochain_service/app/build.gradle.kts
+++ src-tauri/gen/android-holochain_service/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.volla.messages"
         minSdk = 27
         targetSdk = 34
-        versionCode = 12003
-        versionName = "0.12.3"
+        versionCode = 12004
+        versionName = "0.12.4"
         // versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         // versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
