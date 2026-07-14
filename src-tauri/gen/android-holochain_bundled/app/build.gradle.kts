07826820012dec5531eec43f13e6bee3f930dab2
Amirhesp <amir.h.esmailpor@gmail.com>
2026-07-14 15:45:23 +0330

    Build: bump to 0.12.8

--- src-tauri/gen/android-holochain_bundled/app/build.gradle.kts
+++ src-tauri/gen/android-holochain_bundled/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.volla.messages.bundled"
         minSdk = 27
         targetSdk = 34
-        versionCode = 12002
-        versionName = "0.12.2"
+        versionCode = 12008
+        versionName = "0.12.8"
         // versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         // versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
