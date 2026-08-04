83f27834df55019a31d6ac93f6039c9c35ae8b69
Amirhesp <amir.h.esmailpor@gmail.com>
2026-08-04 12:56:48 +0330

    Build: bump to v1.0.1

--- src-tauri/gen/android-holochain_service/app/build.gradle.kts
+++ src-tauri/gen/android-holochain_service/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.volla.messages"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1000000
-        versionName = "1.0.0"
+        versionCode = 1000001
+        versionName = "1.0.1"
         // versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         // versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
