5b9b77450ef83ed23207f276ba4f5ab60dd36898
Amirhesp <amir.h.esmailpor@gmail.com>
2026-09-12 17:57:02 +0330

    build: release 1.0.3

--- src-tauri/gen/android-holochain_bundled/app/build.gradle.kts
+++ src-tauri/gen/android-holochain_bundled/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.volla.messages.bundled"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1000002
-        versionName = "1.0.2"
+        versionCode = 1000003
+        versionName = "1.0.3"
         // versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         // versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
